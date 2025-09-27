64032ca649d5dae640518dcfaf29a2c36c07946f
HassamGani <hag2137@columbia.edu>
2025-09-27 15:03:42 -0400

    loading something

---
+++ CoodYou/CoodYou/CoodYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoodYouApp.swift
+//  CoodYou
+//
+//  Created by Hassam Gani on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CoodYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoodYou/CoodYouTests/CoodYouTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoodYouTests.swift
+//  CoodYouTests
+//
+//  Created by Hassam Gani on 27/09/25.
+//
+
+import Testing
+@testable import CoodYou
+
+struct CoodYouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
