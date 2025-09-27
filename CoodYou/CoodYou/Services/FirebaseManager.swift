24ddf94e167adb83e21cc3f4a8abd8efc72fac0b
Hassam <64437837+HassamGani@users.noreply.github.com>
2025-09-27 15:35:00 -0400

    Move SwiftUI project into CoodYou directory

---
+++ CoodYou/CoodYou/Services/FirebaseManager.swift
@@ -0,0 +1,19 @@
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import FirebaseFunctions
+
+final class FirebaseManager {
+    static let shared = FirebaseManager()
+    let auth: Auth
+    let db: Firestore
+    let functions: Functions
+
+    private init() {
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+        self.auth = Auth.auth()
+        self.db = Firestore.firestore()
+        self.functions = Functions.functions()
+    }
+}
