8908d63a5df517d2ba18b1c008976ee5a743ba6b
Hassam <64437837+HassamGani@users.noreply.github.com>
2025-09-27 21:41:19 -0400

    Configure Google Sign-In client ID

--- CoodYou/CoodYou/CoodYouApp.swift
+++ CoodYou/CoodYou/CoodYouApp.swift
@@ -1,5 +1,8 @@
 import SwiftUI
 import FirebaseCore
+#if canImport(GoogleSignIn)
+import GoogleSignIn
+#endif
 
 @main
 struct CoodYouApp: App {
@@ -21,6 +24,11 @@ struct CoodYouApp: App {
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+#if canImport(GoogleSignIn)
+        if let clientID = FirebaseApp.app()?.options.clientID {
+            GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientID)
+        }
+#endif
         true
     }
 }
